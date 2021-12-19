57e02269626943946359cafae1ca962f1fcf90a4
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-20 00:37:42 +0530

    Create Genes.cpp
    People in Chefland have three different eye colors, namely brown, blue, and green. green is the rarest of the eye colors whereas brown is most common.
    
    The eye color of the child of two people is most likely to be the most common eye color between them.
    
    You are given two characters denoting the eye colors of two people in Chefland. The character R denotes bRown color, B denotes Blue color, and G denotes Green color.
    
    Determine the most likely eye color of their child. (Print R, B or G denoting bRown, Blue or Green respectively).

---
+++ CodeChef/December Starters 20/Genes.cpp
@@ -0,0 +1,61 @@
+Question Link- https://www.codechef.com/START20C/problems/GENE01/
+
+
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	char a, b;
+	cin >> a >> b;
+
+	if(a == 'R') cout << "R" << endl;
+
+	else if(a == 'B') {
+		if(b == 'R') {
+			cout << "R" <<endl;
+		}
+		else if(a == 'B' && b == 'B') {
+			cout << "B" << endl;
+		}
+		else {
+			cout << "B" << endl;
+		} 
+	}
+
+	else if(a == 'G') {
+		if (b == 'R') {
+			cout << "R" << endl;
+		}
+		else if(b == 'B') {
+			cout << "B" << endl;
+		}
+		else {
+			cout << "G" << endl;
+		}
+	}
+
+	return 0;
+}
