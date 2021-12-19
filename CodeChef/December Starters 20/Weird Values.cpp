90a17cf2c11bb4192ade043173009a5b1f61e3b9
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2021-12-20 01:10:14 +0530

    Create Weird Values.cpp
    You are given an array A=[A1,A2,…,AN] containing N integers.
    
    Consider any subarray [AL,AL+1,…,AR] of this subarray (where 1≤L≤R≤N). Ther weirdness of this subarray, denoted w(L,R), is defined to be the number of indices L≤i≤R such that Ai equals the number of occurrences of Ai in this subarray.
    
    Formally, we define w(L,R) for 1≤L≤R≤N as follows:
    
    For every integer x, let fL,R(x) denote the number of indices L≤j≤R such that Aj=x.
    w(L,R) then equals the number of L≤i≤R such that Ai=fL,R(Ai)
    For example, let A=[1,3,5,2,1,2,9,5]. Then,
    
    w(1,8)=2, which can be computed as follows. We have
    f1,8(1)=2
    f1,8(2)=2
    f1,8(3)=1
    f1,8(5)=2
    f1,8(9)=1
    The only indices which satisfy f1,8(Ai)=Ai are i=4 and i=6, and hence w(1,8)=2.
    
    w(3,5)=1 — the subarray is [5,2,1], and each of 1,2,5 occur exactly once in this subarray. Of these, only 1 equals the number of its occurrences in this subarray.
    Given A, your task is to compute the sum of the weirdness of all subarrays of A, i.e, the value
    ∑L=1N∑R=LNw(L,R)

---
+++ CodeChef/December Starters 20/Weird Values.cpp
@@ -0,0 +1,58 @@
+Question Link- https://www.codechef.com/problems/WRDVLS
+
+
+	#include <bits/stdc++.h>
+	#include <cstdio>
+	#include <cstring>
+	#include <cmath>
+	#include <cstring>
+	#include <chrono>
+	#include <complex>
+	#define endl "\n"
+	#define ll long long int
+	#define vi vector<int>
+	#define vll vector<ll>
+	#define vvi vector < vi >
+	#define pii pair<int,int>
+	#define pll pair<long long, long long>
+	#define mod 1000000007
+	#define inf 1000000000000000001;
+	#define all(c) c.begin(),c.end()
+	#define mp(x,y) make_pair(x,y)
+	#define mem(a,val) memset(a,val,sizeof(a))
+	#define eb emplace_back
+	#define f first
+	#define s second
+	
+	using namespace std;
+	int main()
+	{
+		std::ios::sync_with_stdio(false);
+		int T;
+		cin>>T;
+		// cin.ignore(); must be there when using getline(cin, s)
+		while(T--)
+		{
+		int n;
+		cin >> n;
+		vector<vector<int>> inds(n + 1);
+
+		for(int i = 1; i <= n; i++) {
+			int x;
+			cin >> x;
+			if(x <= n) inds[x].push_back(i);
+		}
+
+		long long ans = 0;
+		for(int x = 1; x <= n; x++) {
+			vector<int> indsx = inds[x];
+			indsx.insert(begin(indsx), 0);
+			indsx.push_back(n + 1);
+			for(int i = 1; i < int(size(indsx)) - x; i++) {
+				ans += (long long) x * (indsx[i] - indsx[i-1]) * (indsx[i + x] - indsx[i + x - 1]);
+			}
+		}
+		cout << ans << endl;
+		}
+		return 0;
+	}
