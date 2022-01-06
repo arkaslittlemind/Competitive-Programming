36ebf124628a2bf8eda27a91f135d742086d4d3b
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-06 15:03:51 +0530

    Create 766B Mahmoud and a Triangle.cpp
    Mahmoud has n line segments, the i-th of them has length ai. Ehab challenged him to use exactly 3 line segments to form a non-degenerate triangle. Mahmoud doesn't accept challenges unless he is sure he can win, so he asked you to tell him if he should accept the challenge. Given the lengths of the line segments, check if he can choose exactly 3 of them to form a non-degenerate triangle.
    
    Mahmoud should use exactly 3 line segments, he can't concatenate two line segments or change any length. A non-degenerate triangle is a triangle with positive area.

---
+++ Codeforces/EP 2 Greedy/766B Mahmoud and a Triangle.cpp
@@ -0,0 +1,25 @@
+Question Link- https://codeforces.com/problemset/problem/766/B
+
+//Greedy, Sorting and Implementation //
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i=0;i<n;i++)
+	cin >> arr[i];
+	sort(arr,arr+n);
+	for (int i=1;i<n-1;i++)
+	{
+		if (arr[i-1]+arr[i]>arr[i+1])
+		{
+			cout << "YES";
+			return 0;
+		}
+	}
+	cout << "NO";
+}
