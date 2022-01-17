a2c09d7b70cc318536fe08a8197dbe477515a5b8
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-17 22:40:25 +0530

    Create 141A Amusing Joke.cpp
    So, the New Year holidays are over. Santa Claus and his colleagues can take a rest and have guests at last. When two "New Year and Christmas Men" meet, thear assistants cut out of cardboard the letters from the guest's name and the host's name in honor of this event. Then the hung the letters above the main entrance. One night, when everyone went to bed, someone took all the letters of our characters' names. Then he may have shuffled the letters and put them in one pile in front of the door.
    
    The next morning it was impossible to find the culprit who had made the disorder. But everybody wondered whether it is possible to restore the names of the host and his guests from the letters lying at the door? That is, we need to verify that there are no extra letters, and that nobody will need to cut more letters.
    
    Help the "New Year and Christmas Men" and their friends to cope with this problem. You are given both inscriptions that hung over the front door the previous night, and a pile of letters that were found at the front door next morning.

---
+++ A2OJ Ladder- Codeforces/141A Amusing Joke.cpp
@@ -0,0 +1,106 @@
+Question Link- https://codeforces.com/problemset/problem/141/A
+
+// Implementation //
+
+#include <iostream>
+#include <iosfwd>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <vector>
+#include <bitset>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <stack>
+#include <map>
+#include <deque>
+#include <string>
+#include <list>
+#include <iterator>
+#include <sstream>
+#include <complex>
+#include <fstream>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include <assert.h>
+#include <unordered_map>
+#pragma GCC optimize ("-O3")
+
+using namespace std;
+
+#define watch(x) cout << (#x) << " = " << (x) << endl
+#define PI double(2 * acos(0.0))
+#define LL long long
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+#define INF 1e15
+#define nline "\n"
+
+
+int main()  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3;
+
+    s4 = s1 + s2;
+    sort(s4.begin(), s4.end());
+    sort(s3.begin(), s3.end());
+
+    if (s4 == s3)
+        cout << "YES" << nline;
+    else
+        cout << "NO" << nline;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
