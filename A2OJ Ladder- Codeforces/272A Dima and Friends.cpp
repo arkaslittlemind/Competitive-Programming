e69133baf2930fc861c0c5e27d1169995cbb3197
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-24 20:19:34 +0530

    Create 272A Dima and Friends.cpp
    Dima and his friends have been playing hide and seek at Dima's place all night. As a result, Dima's place got messy. In the morning they decided that they need to clean the place.
    
    To decide who exactly would clean the apartment, the friends want to play a counting-out game. First, all the guys stand in a circle, and then each of them shows some number of fingers on one hand (one to five), and then the boys count in a circle, starting from Dima, the number of people, respective to the total number of fingers shown. The person on who the countdown stops will clean the apartment.
    
    For example, if Dima and one of his friends played hide and seek, and 7 fingers were shown during the counting-out, then Dima would clean the place. If there were 2 or say, 8 fingers shown, then his friend would clean the place.
    
    Dima knows how many fingers each of his friends will show during the counting-out. Now he is interested in the number of ways to show some number of fingers on one hand (one to five), so that he did not have to clean the place. Help Dima.

---
+++ A2OJ Ladder- Codeforces/272A Dima and Friends.cpp
@@ -0,0 +1,68 @@
+Question Link- https://codeforces.com/problemset/problem/272/A
+
+// Implementation //
+
+#include <iostream>
+#include <iosfwd>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <vector>
+#include <bitset>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <stack>
+#include <map>
+#include <deque>
+#include <string>
+#include <list>
+#include <iterator>
+#include <sstream>
+#include <complex>
+#include <fstream>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include <assert.h>
+#include <unordered_map>
+#pragma GCC optimize ("-O3")
+
+using namespace std;
+
+#define watch(x) cout << (#x) << " = " << (x) << endl
+#define PI double(2 * acos(0.0))
+#define LL long long
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+#define INF 1e15
+#define nline "\n"
+
+
+int main()  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, f, total(0), answer(0);
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> f;
+        total += f;
+    }
+    for (int i = 1; i <= 5; ++i)
+    {
+        if ((total + i) % (n + 1) != 1)
+        {
+            answer += 1;
+        }
+    }
+    cout << answer << endl;
+}
