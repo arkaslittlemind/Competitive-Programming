b3e0eefbb914b712b6d91309c507346ed2c9a829
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-01-25 19:56:59 +0530

    Create 450A Jzzhu and Children.cpp
    There are n children in Jzzhu's school. Jzzhu is going to give some candies to them. Let's number all the children from 1 to n. The i-th child wants to get at least ai candies.
    
    Jzzhu asks children to line up. Initially, the i-th child stands at the i-th place of the line. Then Jzzhu start distribution of the candies. He follows the algorithm:
    
        Give m candies to the first child of the line.
        If this child still haven't got enough candies, then the child goes to the end of the line, else the child go home.
        Repeat the first two steps while the line is not empty. 
    
    Consider all the children in the order they go home. Jzzhu wants to know, which child will be the last in this order?

---
+++ A2OJ Ladder- Codeforces/450A Jzzhu and Children.cpp
@@ -0,0 +1,109 @@
+Question Link- https://codeforces.com/problemset/problem/450/A
+
+// Implementation //
+
+#include <iostream>
+#include <iosfwd>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <vector>
+#include <bitset>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <stack>
+#include <map>
+#include <deque>
+#include <string>
+#include <list>
+#include <iterator>
+#include <sstream>
+#include <complex>
+#include <fstream>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include <assert.h>
+#include <unordered_map>
+#pragma GCC optimize ("-O3")
+
+using namespace std;
+
+#define watch(x) cout << (#x) << " = " << (x) << endl
+#define PI double(2 * acos(0.0))
+#define LL long long
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+#define INF 1e15
+#define nline "\n"
+
+
+int main()  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, m, last;
+    double t, max(0);
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> t;
+        if (ceil(t / m) >= max)
+        {
+            last = i;
+            max = ceil(t / m);
+        }
+    }
+
+    cout << last << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
