4b7c139465de6a759b9408bb0a30b1a3f2cdd280
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-02-01 22:27:30 +0530

    Update 224A Parallelepiped.cpp

--- A2OJ Ladder- Codeforces/224A Parallelepiped.cpp
+++ A2OJ Ladder- Codeforces/224A Parallelepiped.cpp
@@ -1,2 +1,59 @@
 Question Link- https://codeforces.com/problemset/problem/224/A
 
+#include <iostream>
+#include <iosfwd>
+#include <iomanip>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <vector>
+#include <bitset>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <stack>
+#include <map>
+#include <deque>
+#include <string>
+#include <list>
+#include <iterator>
+#include <sstream>
+#include <complex>
+#include <fstream>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include <assert.h>
+#include <unordered_map>
+#pragma GCC optimize ("-O3")
+
+using namespace std;
+
+#define watch(x) cout << (#x) << " = " << (x) << endl
+#define PI double(2 * acos(0.0))
+#define LL long long
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+#define INF 1e15
+#define nline "\n"
+
+
+int main()  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    LL x,y,z;
+	cin>>x>>y>>z;
+	int a=sqrt((x*y)/z);
+	int b=sqrt((x*z)/y);
+	int c=sqrt((y*z)/x);
+	LL res=(a+b+c)*4;
+	cout<<res;
+
+}
