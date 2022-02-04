496ce1497e4a9f17d95ba007f152c14638fc71a7
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2022-02-04 23:01:47 +0530

    Create 492B Vanya and Lanterns.cpp
    Vanya walks late at night along a straight street of length l, lit by n lanterns. Consider the coordinate system with the beginning of the street corresponding to the point 0, and its end corresponding to the point l. Then the i-th lantern is at the point ai. The lantern lights all points of the street that are at the distance of at most d from it, where d is some positive number, common for all lanterns.
    
    Vanya wonders: what is the minimum light radius d should the lanterns have to light the whole street?

---
+++ Codeforces/Ransdom Problemset/492B Vanya and Lanterns.cpp
@@ -0,0 +1,47 @@
+Question Link- https://codeforces.com/problemset/problem/492/B
+
+// Implementation & Brute Force //
+
+int a,b,i,m=0;
+
+    cin>>a>>b;
+
+    int ar[100005];
+
+    for(i=0; i<a; i++)
+
+    {
+
+        cin>>ar[i];
+
+    }
+
+    sort(ar,ar+a);
+
+    for(i=0; i<a-1; i++)
+
+    {
+
+        if(ar[i+1]-ar[i]>m)
+
+        {
+
+            m=ar[i+1]-ar[i];
+
+        }
+
+    }
+
+    double p,q,r,ans,ld;
+
+    p=ar[0];
+
+    q=(double)m/2;
+
+    r=max(p,q);
+
+    ld=b-ar[a-1];
+
+    ans=max(r,ld);
+
+    cout<<fixed<<setprecision(10)<<ans<<endl;
