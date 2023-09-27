4195ea7d8569a627eac1f6fe50248fc46b597888
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2023-09-27 22:18:27 +0530

    Update Problem A.cpp

--- CodeChef/Starters 102 Division 4/Problem A.cpp
+++ CodeChef/Starters 102 Division 4/Problem A.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	// your code goes here
+
+	int x;
+
+	cin >> x;
+
+	
+
+	if(x == 8 or x == 9 or x == 10)
+
+	    cout << "yes" << endl;
+
+	    
+
+	 else 
+
+	    cout << "no" << endl;
+
+	
+
+	return 0;
+
+}
+
