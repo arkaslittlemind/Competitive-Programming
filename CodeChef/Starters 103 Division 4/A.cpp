56d4d2421ebdc5a4d6b9673859dce487108c2806
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2023-10-11 21:32:10 +0530

    Update A.cpp

--- CodeChef/Starters 103 Division 4/A.cpp
+++ CodeChef/Starters 103 Division 4/A.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	// your code goes here
+	int x;
+	cin >> x;
+	
+	if(x < 3) {
+	    cout << "GOLD" << endl;
+	}
+	if(x >= 3 && x < 6) {
+	    cout << "SILVER" << endl;
+	}
+	if(x >= 6) {
+	    cout << "BRONZE" << endl;
+	}
+	return 0;
+}
